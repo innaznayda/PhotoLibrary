37baddfa284faeafc601af6ac113373a79a4f11a
innaznayda <innaznayda@gmail.com>
2020-02-19 23:19:58 +0200

    initial view

---
+++ PhotoLibrary/PhotoLibrary/FileController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace PhotoLibrary {
+    static class FileController {
+        static public string GetFileName(string filePath) {
+            if (!File.Exists(filePath))
+                return null;
+
+            return Path.GetFileName(filePath);
+        }
+
+        static public Bitmap GetBitmap(string filePath) {
+            if (!File.Exists(filePath))
+                return null;
+            Bitmap resultBitmap;
+
+            using (FileStream fs = new FileStream(filePath, FileMode.Open)) {
+                byte[] bytes = new byte[fs.Length];
+                fs.Read(bytes, 0, (int)fs.Length);
+                using (MemoryStream ms = new MemoryStream(bytes)) {
+                    resultBitmap = Bitmap.FromStream(ms) as Bitmap;
+                }
+            }
+            return resultBitmap;
+        }
+
+        static public BitmapImage GetBitmapImage(Bitmap bitmap) {
+            BitmapImage resultBitmapImage;
+            Bitmap tempBitmap = new Bitmap(bitmap);
+            if (tempBitmap == null)
+                return null;
+            using (MemoryStream memory = new MemoryStream()) {
+                tempBitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                resultBitmapImage = new BitmapImage();
+                resultBitmapImage.BeginInit();
+                resultBitmapImage.StreamSource = memory;
+                resultBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                resultBitmapImage.EndInit();
+                resultBitmapImage.Freeze();
+            }
+            return resultBitmapImage;
+        }
+
+
+    }
+}
--- PhotoLibrary/PhotoLibrary/MainWindow.xaml.cs
+++ PhotoLibrary/PhotoLibrary/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,26 @@ namespace PhotoLibrary {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        public string DisplayedImagePath {
+            get { return @"C:\Users\Public\Pictures\deal.png"; }
+        }
+
+        // Image Source [BitmapImage]
+        private BitmapImage _displayImage;
+        public BitmapImage DisplayImage {
+            get { return _displayImage; }
+            set { _displayImage = value; }
+        }
+
+        private void GetImageFromFilePath(string filePath) {
+            Bitmap bitmap = FileController.GetBitmap(filePath);
+            DisplayImage = FileController.GetBitmapImage(bitmap);
+        }
+
         public MainWindow() {
             InitializeComponent();
+            DataContext = this;
+            GetImageFromFilePath(DisplayedImagePath);
         }
     }
 }
