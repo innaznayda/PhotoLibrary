0111bc76f3c0bc42766c6ca040f79cd045412636
innaznayda <innaznayda@gmail.com>
2020-02-22 20:40:40 +0200

    drop photo

--- PhotoLibrary/PhotoLibrary/View/PhotoGalleryView.xaml.cs
+++ PhotoLibrary/PhotoLibrary/View/PhotoGalleryView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using PhotoLibrary.Models;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace PhotoLibrary.View {
     /// <summary>
@@ -7,6 +10,12 @@ namespace PhotoLibrary.View {
     public partial class PhotoGalleryView : Window {
         public PhotoGalleryView() {
             InitializeComponent();
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e) {
+            ListBox parent = (ListBox)sender;
+            string data = ((string[])e.Data.GetData(DataFormats.FileDrop, false))[0];
+            (parent.ItemsSource as ObservableCollection<Photo>).Add(new Photo(data));
         }
     }
 }
