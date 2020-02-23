d0b7b2dc1c911f24938b4e382fb044b13e4790cd
innaznayda <innaznayda@gmail.com>
2020-02-23 16:04:56 +0200

    clean up and file filtering

--- PhotoLibrary/PhotoLibrary/View/PhotoGalleryView.xaml.cs
+++ PhotoLibrary/PhotoLibrary/View/PhotoGalleryView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using PhotoLibrary.Models;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
@@ -22,9 +25,41 @@ namespace PhotoLibrary.View {
         private void PhotoGallery_Drop(object sender, DragEventArgs e) {
             ListBox parent = (ListBox)sender;
             var data = ((string[])e.Data.GetData(DataFormats.FileDrop, false));
-            foreach (string file in data) {
+            var files = new AsyncObservableCollection<string>();
+            if (data.Length == 1 && IsFolder(data[0])) {
+                files = CollectFilesInFolderAndFilter(data[0]);
+            } else {
+                files = FilterFiles(data);
+            }
+            foreach (string file in files) {
                 (parent.ItemsSource as AsyncObservableCollection<Photo>).Add(new Photo(file));
             }
         }
+
+        private AsyncObservableCollection<string> CollectFilesInFolderAndFilter(string path) {
+            var files = Directory.GetFiles(path, "*.*");
+
+            return FilterFiles(files);
+        }
+
+        private AsyncObservableCollection<string> FilterFiles(string[] files) {
+            return files
+            .Where(s => s.EndsWith(".BMP", StringComparison.InvariantCultureIgnoreCase)
+                     || s.EndsWith(".GIF", StringComparison.InvariantCultureIgnoreCase)
+                     || s.EndsWith(".EXIF", StringComparison.InvariantCultureIgnoreCase)
+                     || s.EndsWith(".JPG", StringComparison.InvariantCultureIgnoreCase)
+                     || s.EndsWith(".PNG", StringComparison.InvariantCultureIgnoreCase)
+                     || s.EndsWith(".TIFF", StringComparison.InvariantCultureIgnoreCase))
+                     .ToObservableCollection();
+        }
+
+        private bool IsFolder(string path) {
+            FileAttributes attr = File.GetAttributes(path);
+            if (attr.HasFlag(FileAttributes.Directory))
+                return true;
+            return false;
+        }
+
+
     }
 }
